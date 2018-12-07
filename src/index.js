890127dbbd5d522f7bbb8dee04fdd372b48a1021
Jupiter191 <liam.h.-@hotmail.com>
2018-12-07 21:56:26 +0000

    Removed duplicate use of serviceWorker. Used import rather than path literal

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './app/layout/App';
 import * as serviceWorker from './serviceWorker';
 
 const rootElement = document.getElementById('root');
@@ -11,14 +11,12 @@ let render = function(){
 };
 
 if(module.hot){
-    module.hot.accept('./App', function(){
+    module.hot.accept(App, function(){
         setTimeout(render);
     });
 }
 
 render();
-serviceWorker.register();
-
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
