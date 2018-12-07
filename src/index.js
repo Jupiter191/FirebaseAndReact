dc4ff1184aa7532ff601ed8707e17a4c7e9b7c74
Jupiter191 <liam.h.-@hotmail.com>
2018-12-07 21:48:21 +0000

    Added hot loading

--- src/index.js
+++ src/index.js
@@ -4,7 +4,21 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById('root');
+
+let render = function(){
+    ReactDOM.render(<App />, rootElement);
+};
+
+if(module.hot){
+    module.hot.accept('./App', function(){
+        setTimeout(render);
+    });
+}
+
+render();
+serviceWorker.register();
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
